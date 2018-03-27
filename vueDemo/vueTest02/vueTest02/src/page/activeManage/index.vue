5c4ad175ee00a5934295802c3f4c662a737ab1e2
DESKTOP-DJCQ2A6\cbw <861754186@qq.com>
2018-03-27 15:35:14 +0800

    增加一部分vue的基本操作

--- vueDemo/vueTest02/vueTest02/src/page/activeManage/index.vue
+++ vueDemo/vueTest02/vueTest02/src/page/activeManage/index.vue
@@ -11,6 +11,44 @@
     </li>
   </ol>
 
+  <hr/>
+  <h1> 基本介绍 </h1>
+
+ <table>
+   <li> v-text</li>
+   <li> v-if</li>
+   <li> v-bind</li>
+   <li> v-on</li>
+
+
+ </table>
+
+  <hr/>
+
+  <h1>v-on</h1>
+  这里写v-on的一些方法,v-on可以简写成@click
+  貌似第一种方法已经不行了
+  <!--<button v-on:click="doThis">这里使用的是v-on</button>-->
+  <button @click="doThis">这里使用的是"@"</button>
+
+  <hr/>
+  <h1>v-bind</h1>
+
+  绑定事件,实现双向绑定
+
+  <hr/>
+  <h1>存储功能</h1>
+
+
+  <input type="text" v-bind:value="storeValue"/>
+
+
+
+
+
+
+
+
 </div>
 
 </template>
@@ -19,19 +57,32 @@
   export default {
     name: "index",
     data(){
-     return{
-       todos:[
-
-         {text : "学好vue"},
-         {text:"认真做好开发"},
-         {text:"早日建设好自己的网站后台"},
-         {text:"前台页面的事情以后处理"},
-         {text:"关键认清楚方向"}
-
-
-       ]
+     return {
+       todos: [
+          //这里使用的是v-for进行渲染
+         {text: "学好vue"},
+         {text: "认真做好开发"},
+         {text: "早日建设好自己的网站后台"},
+         {text: "前台页面的事情以后处理"},
+         {text: "关键认清楚方向"}
+       ],
+       storeValue :"123"
      }
+    },
 
+    methods:{
+      //这里写常用的一些方法
+
+      doThis(){
+
+        alert("doThis")
+
+      }
+
+    },
+
+    created:{
+      //这里写一些create的方法
 
     }
   }
